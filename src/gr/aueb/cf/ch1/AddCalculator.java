858f06e2a4d1569de245118863369a62ab237b07
Christopher Kallimachos <ckallimachos@gmail.com>
2024-12-16 19:42:51 +0200

    project created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int a = 0;
+    }
+}
---
+++ src/gr/aueb/cf/ch2/AddCalculator2.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.ch2;
+
+public class AddCalculator2 {
+
+    public static void main(String[] args) {
+        int a = 2;
+    }
+}
