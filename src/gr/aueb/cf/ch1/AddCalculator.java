35fafe988a1fe3b10505d667d75667bb671b291d
Christopher Kallimachos <ckallimachos@gmail.com>
2024-12-16 20:01:23 +0200

    hotfix println added

--- src/gr/aueb/cf/ch1/AddCalculator.java
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -3,6 +3,16 @@ package gr.aueb.cf.ch1;
 public class AddCalculator {
 
     public static void main(String[] args) {
-        int a = 0;
+        int num1 = 8;
+        int num2 = 2;
+        int result = 0;
+
+        result = add(num1 , num2);
+
+        System.out.println("Sum: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
---
+++ src/gr/aueb/cf/ch4/DivCalculator.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.ch4;
+
+public class DivCalculator {
+
+    public static void main(String[] args) {
+        int a = 0;
+    }
+}
